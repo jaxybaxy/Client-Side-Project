351dce22e017a2a29205fd1550a3f52910ff89e4
jaxybaxy <momen.ahmed_2010@yahoo.com>
2024-02-17 19:18:56 +0200

    add: cart in local storage

--- js/nav.js
+++ js/nav.js
@@ -1,9 +1,28 @@
-const cartNum = document.getElementById("cart-number");
-export function cartNumber(cartNum) {
+// const cartNum = document.getElementById("cart-number");
+// export function cartNumber(cartNum) {
+//   if (localStorage.getItem("cartItems")) {
+//     // console.log(localStorage.getItem("cartItems") );
+
+//     cartNum.innerText = JSON.parse(localStorage.getItem("cartItems")).length;
+//   } else {
+//     cartNum.innerText = 0;
+//   }
+// }
+
+// product.js
+
+export function cartNumber() {
+  const cartNumElement = document.getElementById("cart-number");
   if (localStorage.getItem("cartItems")) {
-    cartNum.innerText = JSON.parse(localStorage.getItem("cartItems")).length;
+    const cartItems = JSON.parse(localStorage.getItem("cartItems"));
+    const cartLength = cartItems.reduce(
+      (total, item) => total + item.quantity,
+      0
+    );
+    cartNumElement.innerText = cartLength;
   } else {
-    cartNum.innerText = 0;
+    cartNumElement.innerText = 0;
   }
 }
+
 
--- js/products.js
+++ js/products.js
@@ -1,165 +1,184 @@
+import { cartNumber } from "./nav.js";
 
-// import cartNumebr from './nav.js';
-const prodCont = document.querySelector(".prodContainer");
-const searchParams = new URLSearchParams(window.location.search);
-let category = searchParams.get('category'); // price_descending
+document.addEventListener("DOMContentLoaded", function () {
+  cartNumber();
 
-document.title = category ;
+  const prodCont = document.querySelector(".prodContainer");
+  const searchParams = new URLSearchParams(window.location.search);
+  let category = searchParams.get("category"); // price_descending
 
-fetch(`https://dummyjson.com/products/category/${category}`)
-  .then((res) => res.json())
-  .then((products) => {
-    products.products.forEach((element) => {
-      //Product card
-      prodCard = document.createElement("div");
-      prodCard.classList.add("prodCard");
+  document.title = category;
 
-      // //Product name
-      // prodTitle = document.createElement("p");
-      // prodTitle.innerText = element.title;
-      // prodTitle.classList.add("prodTitle");
-      // prodCard.appendChild(prodTitle);
-      //Product image
-      prodImage = document.createElement("img");
-      prodImage.src = element.thumbnail;
-      prodImage.classList.add("prodImage");
-      prodCard.appendChild(prodImage);
-      
+  fetch(`https://dummyjson.com/products/category/${category}`)
+    .then((res) => res.json())
+    .then((products) => {
+      products.products.forEach((element) => {
+        //Product card
+        let prodCard = document.createElement("div");
+        prodCard.classList.add("prodCard");
 
+        // //Product name
+        // prodTitle = document.createElement("p");
+        // prodTitle.innerText = element.title;
+        // prodTitle.classList.add("prodTitle");
+        // prodCard.appendChild(prodTitle);
+        //Product image
+        let prodImage = document.createElement("img");
+        prodImage.src = element.thumbnail;
+        prodImage.classList.add("prodImage");
+        prodCard.appendChild(prodImage);
 
-
-      
         //Product description
-        prodDesc = document.createElement('p');
+        let prodDesc = document.createElement("p");
         prodDesc.innerText = element.description;
         prodDesc.classList.add("prodDesc");
         prodCard.appendChild(prodDesc);
 
-      //Product price
-      prodPrice = document.createElement("p");
-      prodPrice.innerText = element.price + "$";
-      // prodPrice.classList.add("prodPrice");
+        //Product price
+        let prodPrice = document.createElement("p");
+        prodPrice.innerText = element.price + "$";
+        // prodPrice.classList.add("prodPrice");
 
-      //Product Discount
-      prodDscnt = document.createElement("p");
-      discount = Math.ceil(
-        element.price - element.price * (element.discountPercentage / 100)
-      );
-      prodDscnt.innerText = discount + "$";
+        //Product Discount
+        let prodDscnt = document.createElement("p");
+        let discount = Math.ceil(
+          element.price - element.price * (element.discountPercentage / 100)
+        );
+        prodDscnt.innerText = discount + "$";
 
-      // prodSale = document.createElement("p");
-      // prodSale.innerText = Math.ceil(element.discountPercentage) + "%";
-
-      priceDiv = document.createElement("div");
-      priceDiv.appendChild(prodPrice);
-      priceDiv.appendChild(prodDscnt);
-      prodCard.appendChild(priceDiv);
-
-      priceDiv.classList.add("priceDiv");
-
-      //Product div styling
-
-      if (element.discountPercentage >= 10) {
-        prodDscnt.classList.add("prodDscnt");
-        prodPrice.classList.add("prodPrice");
         // prodSale = document.createElement("p");
-        // prodSale.innerHTML = `<p>${Math.ceil(element.discountPercentage)}%</p>`;
-        // prodSale.classList.add("prodSale");
-        // prodCard.appendChild(prodSale);
-      } else {
-        prodPrice.style.fontSize = "2rem";
-        prodPrice.classList.add("prodDscnt");
-        prodDscnt.style.display = "none";
-      }
+        // prodSale.innerText = Math.ceil(element.discountPercentage) + "%";
 
-      //// Product rating start
-      let rate = (element.rating / 5) * 100;
+        let priceDiv = document.createElement("div");
+        priceDiv.appendChild(prodPrice);
+        priceDiv.appendChild(prodDscnt);
+        prodCard.appendChild(priceDiv);
 
-      //&#9733;
-      // const fullStar = `
-      //   <div class="fullStar" style="width:${rate}%">
-      //   <span>★★★★★</span>
-      //   </div>
-      //   `;
-      // const emptyStar = `
-      //   <div class="emptyStar">
-      //   <span>★★★★★</span>
-      //   </div>
-     
-      //   `;
-      // prodRating = document.createElement("div");
-      // prodRating.innerHTML = emptyStar + fullStar;
-      // prodRating.classList.add("prodRating");
-      // prodRating.classList.add("star-ratings");
+        priceDiv.classList.add("priceDiv");
 
-      // //Product rating number
-      // const rateNum = document.createElement("p");
-      // rateNum.innerText = element.rating;
-      // rateNum.classList.add("rateNum");
+        //Product div styling
 
-      // //Product rating div
-      // const ratingDiv = document.createElement("div");
-      // ratingDiv.appendChild(prodRating);
-      // ratingDiv.appendChild(rateNum);
-      // prodCard.appendChild(ratingDiv);
-      // ratingDiv.classList.add("ratingDiv");
+        if (element.discountPercentage >= 10) {
+          prodDscnt.classList.add("prodDscnt");
+          prodPrice.classList.add("prodPrice");
+          // prodSale = document.createElement("p");
+          // prodSale.innerHTML = `<p>${Math.ceil(element.discountPercentage)}%</p>`;
+          // prodSale.classList.add("prodSale");
+          // prodCard.appendChild(prodSale);
+        } else {
+          prodPrice.style.fontSize = "2rem";
+          prodPrice.classList.add("prodDscnt");
+          prodDscnt.style.display = "none";
+        }
 
-//       //Product Order Quantity
-//       // console.log(element.stock);
-//       prodQuant = document.createElement("div");
-//       // <p> In Stock : ${element.stock}</P>
-//       prodQuant.innerHTML = `
+        //// Product rating start
+        let rate = (element.rating / 5) * 100;
 
-//         <button class="downQuant" onClick='decreaseCount(event, this , ${element.stock})'>▼</button>
-//         <span class="quantSpan">1</span>
-//         <button class="upQuant" onClick='increaseCount( event,this , ${element.stock})'>▲</button>
-// `;
-//       prodQuant.classList.add("prodQuant");
-//       prodCard.appendChild(prodQuant);
+        //&#9733;
+        // const fullStar = `
+        //   <div class="fullStar" style="width:${rate}%">
+        //   <span>★★★★★</span>
+        //   </div>
+        //   `;
+        // const emptyStar = `
+        //   <div class="emptyStar">
+        //   <span>★★★★★</span>
+        //   </div>
 
-      //Add to cart button
-      add2Cart = document.createElement("button");
-      add2Cart.value = 1;
-      add2Cart.innerText = "Add To Cart";
-      add2Cart.classList.add("add2Cart");
-      add2Cart.setAttribute("id", element.id);
-      prodCard.appendChild(add2Cart);
+        //   `;
+        // prodRating = document.createElement("div");
+        // prodRating.innerHTML = emptyStar + fullStar;
+        // prodRating.classList.add("prodRating");
+        // prodRating.classList.add("star-ratings");
 
+        // //Product rating number
+        // const rateNum = document.createElement("p");
+        // rateNum.innerText = element.rating;
+        // rateNum.classList.add("rateNum");
 
-      // Product details
-      prodDetails = document.createElement('a');
-      prodDetails.innerText = 'View product details';
-      prodDetails.classList.add('prodDetails');
-      prodDetails.href= `snglprod.html?id=${element.id}`;
-      add2Cart.insertAdjacentElement("beforeBegin" , prodDetails);
+        // //Product rating div
+        // const ratingDiv = document.createElement("div");
+        // ratingDiv.appendChild(prodRating);
+        // ratingDiv.appendChild(rateNum);
+        // prodCard.appendChild(ratingDiv);
+        // ratingDiv.classList.add("ratingDiv");
 
-      prodCont.appendChild(prodCard);
-    });
-    cartButtons = document.getElementsByClassName("add2Cart");
-    for (i = 0; i < cartButtons.length; i++) {
-        // console.log(cartButtons[i].id);
+        //       //Product Order Quantity
+        //       // console.log(element.stock);
+        //       prodQuant = document.createElement("div");
+        //       // <p> In Stock : ${element.stock}</P>
+        //       prodQuant.innerHTML = `
+
+        //         <button class="downQuant" onClick='decreaseCount(event, this , ${element.stock})'>▼</button>
+        //         <span class="quantSpan">1</span>
+        //         <button class="upQuant" onClick='increaseCount( event,this , ${element.stock})'>▲</button>
+        // `;
+        //       prodQuant.classList.add("prodQuant");
+        //       prodCard.appendChild(prodQuant);
+
+        //Add to cart button
+        let add2Cart = document.createElement("button");
+        add2Cart.value = 1;
+        add2Cart.innerText = "Add To Cart";
+        add2Cart.classList.add("add2Cart");
+        add2Cart.setAttribute("id", element.id);
+        prodCard.appendChild(add2Cart);
+
+        // Product details
+        let prodDetails = document.createElement("a");
+        prodDetails.innerText = "View product details";
+        prodDetails.classList.add("prodDetails");
+        prodDetails.href = `snglprod.html?id=${element.id}`;
+        add2Cart.insertAdjacentElement("beforeBegin", prodDetails);
+
+        prodCont.appendChild(prodCard);
+      });
+      // cartButtons = document.getElementsByClassName("add2Cart");
+      // for (i = 0; i < cartButtons.length; i++) {
+      //   // console.log(cartButtons[i].id);
+      //   cartButtons[i].addEventListener("click", (event) => {
+      //     if (!localStorage.getItem("cartItems")) {
+      //       cart = [];
+      //       cart.push(event.target.id);
+      //       localStorage.setItem("cartItems", JSON.stringify(cart));
+      //     } else {
+      //       cart = JSON.parse(localStorage.getItem("cartItems"));
+      //       cartIDs = [];
+      //       cart.forEach((item) => {
+      //         console.log(item);
+      //         cartIDs.push(item);
+      //         console.log(cartIDs);
+      //       });
+      //       if (!cartIDs.includes(event.target.id)) {
+      //         cart.push(event.target.id);
+      //         localStorage.setItem("cartItems", JSON.stringify(cart));
+      //       }
+      //     }
+      //     // cartNumebr();
+      //   });
+      // }
+      const cartButtons = document.getElementsByClassName("add2Cart");
+      for (let i = 0; i < cartButtons.length; i++) {
         cartButtons[i].addEventListener("click", (event) => {
+          const itemId = event.target.id;
+          let cart = [];
 
-          if (!localStorage.getItem('cartItems')){
-              cart = [];
-              cart.push(event.target.id);
-              localStorage.setItem("cartItems", JSON.stringify(cart));
+          if (localStorage.getItem("cartItems")) {
+            cart = JSON.parse(localStorage.getItem("cartItems"));
+          }
+
+          let itemIndex = cart.findIndex((item) => item.id === itemId);
+          if (itemIndex !== -1) {
+            // If item already exists in cart, increase its quantity
+            cart[itemIndex].quantity += 1;
           } else {
-             cart = JSON.parse(localStorage.getItem('cartItems'));
-             cartIDs = [];
-             cart.forEach((item)=>{
-              console.log(item);
-              cartIDs.push(item);
-              console.log(cartIDs);
-             })
-              if (!(cartIDs.includes(event.target.id))){
-                cart.push(event.target.id)
-                localStorage.setItem("cartItems", JSON.stringify(cart));
-              }
+            // If item does not exist in cart, add it with quantity 1
+            cart.push({ id: itemId, quantity: 1 });
+          }
 
-            }
-            // cartNumebr();
+          localStorage.setItem("cartItems", JSON.stringify(cart));
+          cartNumber();
         });
-      };
-  });
-
+      }
+    });
+});
