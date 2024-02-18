8afff1834bd8e70a211d6e7814edeea2f1beae30
jaxybaxy <momen.ahmed_2010@yahoo.com>
2024-02-18 21:06:03 +0200

    fix: two nav bars

--- js/product-cat.js
+++ js/product-cat.js
@@ -1,4 +1,4 @@
-let divparent = document.getElementById("all-cat-container");
+let divparent = document.getElementById("all-cat-container-nav")||document.getElementById("all-cat-container");
 let allcat1 = fetch("https://dummyjson.com/products/categories")
   .then((res) => res.json())
   .then((outputjson) => {
